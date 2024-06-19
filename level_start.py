e5a48085ea67609a0aeb51b0a98d5edd2e71d193
Miki <mikolaj.puchala1@student.up.krakow.pl>
2024-06-19 02:56:19 +0200

    dokumentacja

--- level_start.py
+++ level_start.py
@@ -2,10 +2,15 @@ from level import Level
 
 class Level_start(Level):
     def __init__(self, player, images, entry_door_direction=None):
-        super().__init__(player, images, entry_door_direction)
-        self.images = images
-        self.obstacles = [
-        ]
+        """
+        Inicjalizuje poziom początkowy.
 
-        player.level = self
+        :param player: Obiekt gracza
+        :param images: Zestaw obrazów poziomu
+        :param entry_door_direction: Kierunek drzwi wejściowych
+        """
+        super().__init__(player, images, entry_door_direction)  # inicjalizacja klasy bazowej
+        self.images = images  # przypisanie obrazów poziomu
+        self.obstacles = []  # lista przeszkód na poziomie (początkowo pusta)
 
+        player.level = self  # ustawienie poziomu dla gracza
